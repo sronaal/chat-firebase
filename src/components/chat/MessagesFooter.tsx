caeb0ff42baa4e5af783b557756ffe5ede67b561
sronaal <ronal.sanx21@gmail.com>
2024-09-04 18:32:10 -0500

    Picker Emoji para los mensajes

--- src/components/chat/MessagesFooter.tsx
+++ src/components/chat/MessagesFooter.tsx
@@ -1,21 +1,42 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import EmojiPicker from 'emoji-picker-react';
+
 import { FaRegSmileBeam } from "react-icons/fa";
 import { IoSend } from "react-icons/io5";
 
 export const MessageFooter = () => {
 
+
+    const [message, setMessage] = useState("")
+    const [EmojiPickerOpen, setEmojiPickerOpen] = useState(false)
+
+    const handleSendMessage = async () => {
+
+        console.log(message)
+    }
+
+
+
+
     return (
 
-        <footer className="border-t p-4 flex gap-x-2">
-
-            <Button>
-                <FaRegSmileBeam className="text-xl"/>
+        <footer className="border-t p-4 flex gap-x-2 relative">
+            <Button onClick={() => setEmojiPickerOpen((prev => !prev))}>
+                <FaRegSmileBeam className="text-xl" />
             </Button>
+            <div className="absolute bottom-12">
 
-            <Input placeholder="Type a messsage"/>
-            <Button>
-                <IoSend className="text-xl"/>
+                <EmojiPicker open={ EmojiPickerOpen } />
+            </div>
+            <Input
+                placeholder="Type a messsage"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <Button onClick={handleSendMessage}>
+                <IoSend className="text-xl" />
             </Button>
         </footer>
     )
