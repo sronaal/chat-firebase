1847f00c74d99fbf607c42aaf7f0bff8f3ae1085
sronaal <ronal.sanx21@gmail.com>
2024-09-04 16:09:21 -0500

    Solucion props friend list

--- src/components/chat/Friend-item.tsx
+++ src/components/chat/Friend-item.tsx
@@ -1,18 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-const FriendItem = ({displayName,lastMessage,photoURL}) => {
+const FriendItem = (props : any) => {
     return (
         <div>
 
 
             <article className="flex items-center gap-x-4 py-4 px-5 border-b hover:bg-gray-100 cursor-pointer">
                 <img
-                    src={photoURL}
+                    src={props.photoURL}
                     alt=""
                     className="w-12 h-12 rounded-md"
                 />
                 <div className="flex-1 min-w-0">
-                    <h3 className="font-semibold text-lg text-gray-800">{ displayName }</h3>
-                    <p className="text-sm text-gray-800 truncate"> { lastMessage }</p>
+                    <h3 className="font-semibold text-lg text-gray-800">{ props.displayName }</h3>
+                    <p className="text-sm text-gray-800 truncate"> { props.lastMessage }</p>
                 </div>
             </article>
             
